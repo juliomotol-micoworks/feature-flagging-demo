f483dec9f63de91277b1597ea64aa436250dd73e
Julio Motol <julio.motol89@gmail.com>
2024-10-21 21:43:59 +0800

    Base
    Signed-off-by: Julio Motol <julio.motol89@gmail.com>

---
+++ config/features.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'foo' => (bool) env('FEATURE_FOO', false),
+    'bar' => (bool) env('FEATURE_BAR', false),
+    'baz' => (bool) env('FEATURE_BAZ', false),
+];
---
+++ tests/Feature/FeatureMigrationsTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Providers\AppServiceProvider;
+
+it('can run migrations', function (string $feature, string $migrationName) {
+    $this->app['config']->set("features.{$feature}", false);
+
+    $this->app->register(AppServiceProvider::class, true);
+
+    $this->artisan('migrate:fresh');
+
+    if ($migrationName) {
+        expect($this->app['migration.repository']->getRan())
+            ->not
+            ->toContain($migrationName);
+    }
+
+    $this->app['config']->set("features.{$feature}", true);
+
+    $this->app->register(AppServiceProvider::class, true);
+
+    $this->artisan('migrate');
+
+    if ($migrationName) {
+        expect($this->app['migration.repository']->getRan())
+            ->toContain($migrationName);
+    }
+})
+    ->with([
+        'foo' => ['foo', '2024_10_21_131020_create_foo_table'],
+        'bar' => ['bar', '2024_10_21_131104_create_bar_table'],
+        // baz doesn't have any migrations to run so no need to assert
+    ]);
